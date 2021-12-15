ad19631aa738e61ada15a6c9104564df1b1a02b1
German Almonte <german.a.almonte@gmail.com>
2021-12-14 23:22:34 -0500

    Fixed player animation so that he can run properly, Ogres are now animated again.

--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -20,13 +20,18 @@ export default class MyGame extends Phaser.Scene
 
     create ()
     {
+    // MAP
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("tf_jungle_tileset", "tiles");
     map.createLayer("Background", tileset, 0, 0);
     const blockedLayer = map.createLayer("Blocked", tileset, 0, 0);
     blockedLayer.setCollisionByExclusion(-1, true);
-    this.player = new Player(this,225,343, 'knight').setScale(2)
+
+    // PLAYER
+    this.player = new Player(this,225,343, 'knight').setScale(4)
     this.player.body.setCollideWorldBounds(true)
+
+    // TROLLS
     this.troll = new Troll(this,360, 50, "troll-enemy").setScale(3)
     this.troll = new Troll(this,90, 50, "troll-enemy").setScale(3)
     this.troll = new Troll(this,240, 50, "troll-enemy").setScale(3)
--- src/js/modules/troll.js
+++ src/js/modules/troll.js
@@ -11,34 +11,27 @@ export default class Troll extends Entity{
         this.type = type
         this.speed = speed
         anims.create({
-            key: "walk",
+            key: "trollwalk",
             frames: anims.generateFrameNames("troll-enemy", {
               start: 1,
               end: 4,
-              zeroPad: 1,
               prefix: "troll_walk_",
             }),
             frameRate: 8,
             repeat: -1,
           });
-        anims.create({
-            key: "idle",
+          anims.create({
+            key: "trollidle",
             frames: anims.generateFrameNames("troll-enemy", {
               start: 1,
               end: 4,
-              zeroPad: 1,
               prefix: "troll_idle_",
             }),
             frameRate: 8,
             repeat: -1,
           });
+          this.anims.play("trollidle")
     }
     update(){
-        this.anims.play("idle",true)
-        // this.body.setVelocity(0)
-
-        // if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
-        //     this.anims.play('idle', true)
-        // } 
     }
 }
