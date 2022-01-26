26303c332a68c6ad8674dd8bcf69c3d85f4a8f92
Omar <Omar>
2022-01-26 16:00:32 -0500

    fixed collision with the objects and depth behind objects

--- src/js/config.js
+++ src/js/config.js
@@ -1,17 +1,19 @@
-import Phaser from 'phaser'
- 
+import Phaser from "phaser";
+
 export const config = {
-    type: Phaser.AUTO,
-    width: 400,
-    height: 225,
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { y: 0 },
-      },
+  type: Phaser.AUTO,
+  width: 400,
+  height: 225,
+  parent: "phaser-game",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
     },
-    scale: {
-      mode: Phaser.Scale.FIT,
-      autoCenter: Phaser.Scale.CENTER_BOTH,},
-    pixelArt: true,
-  };
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
+};
--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -1,56 +1,55 @@
-import Phaser from 'phaser'
-import Player from './player.js'
-import Troll from './troll.js';
+import Phaser from "phaser";
+import Player from "./player.js";
+import Troll from "./troll.js";
 
+export default class MyGame extends Phaser.Scene {
+  constructor() {
+    super("thisGame");
+  }
 
-export default class MyGame extends Phaser.Scene
-{
-    
-    constructor ()
-    {
-    super('thisGame');
+  preload() {
+    this.load.image("tiles", "./src/img/map/tf_jungle_tileset.png");
+    this.load.tilemapTiledJSON("map", "./src/tilemaps/Map.json");
+    this.player;
+    this.keys;
+    this.troll;
+    this.cameras;
+  }
 
-    }
-
-    preload ()
-    {
-    this.player
-    this.keys
-    this.troll 
-    this.cameras
-    }
-
-    create ()
-    {
+  create() {
     // MAP
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("tf_jungle_tileset", "tiles");
-    map.createLayer("Background", tileset, 0, 0);
+    const backgroundLayer = map.createLayer("Background", tileset, 0, 0);
     const blockedLayer = map.createLayer("Blocked", tileset, 0, 0);
-    blockedLayer.setCollisionByExclusion(-1, true);
+    const overheadLayer = map.createLayer("Overhead", tileset, 0, 0);
+
+    // this.player.setDepth(10);
+    overheadLayer.setDepth(20);
 
     // CAMERA
-    this.physics.world.bounds.width = map.widthInPixels
-    this.physics.world.bounds.height = map.heightInPixels
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+    this.physics.world.bounds.width = map.widthInPixels;
+    this.physics.world.bounds.height = map.heightInPixels;
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.setRoundPixels(true);
-    
-
 
     // PLAYER
-    this.player = new Player(this,225,343, 'knight').setScale(1);
+    this.player = new Player(this, 225, 343, "knight").setScale(1);
     this.player.body.setCollideWorldBounds(true);
-    this.cameras.main.startFollow(this.player, true, 0.1, 0.1)
-    
+    this.cameras.main.startFollow(this.player, true, 0.1, 0.1);
 
     // TROLLS
-    this.troll = new Troll(this,360, 50, "troll-enemy").setScale(1);
-    this.troll = new Troll(this,90, 50, "troll-enemy").setScale(1);
-    this.troll = new Troll(this,240, 50, "troll-enemy").setScale(1.5);
+    this.troll = new Troll(this, 360, 50, "troll-enemy").setScale(1);
+    this.troll = new Troll(this, 90, 50, "troll-enemy").setScale(1);
+    this.troll = new Troll(this, 240, 50, "troll-enemy").setScale(1.5);
     this.troll.body.setCollideWorldBounds(true);
-    }
 
-    update(time,delta){
-        this.player.update()
-    }
+    blockedLayer.setCollisionByProperty({ collide: true });
+    this.physics.add.collider(this.player, blockedLayer);
+    this.physics.add.collider(this.player, this.troll);
+  }
+
+  update(time, delta) {
+    this.player.update();
+  }
 }
