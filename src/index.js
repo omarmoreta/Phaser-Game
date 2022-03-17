844ed0740e08fb63dfc6197d4393fdfdceb1507a
Omar <o.moreta@gmail.com>
2022-03-17 00:43:55 -0400

    new webpack config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import Phaser from 'phaser'
-
-import {config} from './js/config.js'
-import MyGame from "./js/modules/game.js"
-import BootScene from './js/modules/bootscene.js'
-import MainmenuScene from './js/modules/mainmenu.js'
-import UI from './js/modules/uiscene.js'
-import PauseScene from './js/modules/pause.js'
-import PauseScreen from './js/modules/pausescreen.js'
+import Phaser from "phaser";
+import { config } from "./js/config.js";
+import MyGame from "./js/modules/game.js";
+import BootScene from "./js/modules/bootscene.js";
+import MainmenuScene from "./js/modules/mainmenu.js";
+import UI from "./js/modules/uiscene.js";
+import PauseScene from "./js/modules/pause.js";
+import PauseScreen from "./js/modules/pausescreen.js";
 
 class Game extends Phaser.Game {
-  constructor(){
+  constructor() {
     super(config);
-    this.scene.add('thisGame', MyGame);
-    this.scene.add('Boot', BootScene);
-    this.scene.add('Mainmenu', MainmenuScene);
-    this.scene.add('Interface', UI)
-    this.scene.add('Pause', PauseScene);
-    this.scene.add('PScreen', PauseScreen);
-    this.scene.start('Boot');
+    this.scene.add("thisGame", MyGame);
+    this.scene.add("Boot", BootScene);
+    this.scene.add("Mainmenu", MainmenuScene);
+    this.scene.add("Interface", UI);
+    this.scene.add("Pause", PauseScene);
+    this.scene.add("PScreen", PauseScreen);
+    this.scene.start("Boot");
   }
 }
-window.onload = function(){
+window.onload = function () {
   window.game = new Game();
-}
+};
--- src/js/modules/pause.js
+++ src/js/modules/pause.js
@@ -1,31 +1,27 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
 var pausekey;
 var resumekey;
 
 export default class PauseScene extends Phaser.Scene {
-    constructor(){
-    super('Pause');
+  constructor() {
+    super("Pause");
     this.paused = false;
-
-    }
-    preload(){
-    }
-    create(){
+  }
+  preload() {}
+  create() {
     pausekey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     resumekey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
-
-}
-    update(){
-        if (pausekey.isDown && this.paused === false) {
-            this.paused = true;
-            this.scene.pause('thisGame');
-            this.scene.launch('PScreen');}
-        else if(pausekey.isDown.once && this.paused === true){
-            this.paused = false;
-            this.scene.resume('thisGame');
-            this.scene.stop('PScreen');}
-        };
-
-
+  }
+  update() {
+    if (pausekey.isDown && this.paused === false) {
+      this.paused = true;
+      this.scene.pause("thisGame");
+      this.scene.launch("PScreen");
+    } else if (resumekey.isDown && this.paused === true) {
+      this.paused = false;
+      this.scene.resume("thisGame");
+      this.scene.stop("PScreen");
+    }
+  }
 }
